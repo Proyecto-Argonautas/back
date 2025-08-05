589699db5ebb54ed6711d3819a5d341faff99a2c
Autovot <87210193+Autovot@users.noreply.github.com>
2025-08-06 01:05:14 +0200

    Edited schema travel, removed uuid required

--- server/schemas/travelSchema.ts
+++ server/schemas/travelSchema.ts
@@ -15,5 +15,5 @@ export const travelSchema = z.object({
 );
 
 export const getFilteredTravelsSchema = z.object({
-    userId: z.string().uuid(),
+    userId: z.string().min(1, "El userId es obligatorio"),
 });
